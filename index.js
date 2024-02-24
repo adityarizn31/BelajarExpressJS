472ccea4e114fdff16950df7786e6fc5ae3baa4b
Aditya Rizkiawan <@adityarizkiawann@gmail.com>
2024-02-24 08:15:05 +0700

    Mengenal Object dan Response

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require ('express'); // Membuat objek terlebih dahulu
 
 const app = express(); 
 
-app.use(() => {
+app.use((req, res) => { // ini merupakan object dan response
   console.log('we got request');
+  // console.dir(req);
+  // console.dir(res);
+  res.send('<h1>Hello Express </h1>');
+  res.send ({ message: 'Hello World' });
+  res.send ("Ini String dari Respon");
 });
 
 app.listen(8080, ()=> { // Digunakna untuk menjalankan method main utama 
